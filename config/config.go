77ad8b10d1586507cca081390f39ad7f6b09a5fe
Amar <amar.unpak@gmail.com>
2022-11-14 09:39:13 +0700

    pkg, schemas

---
+++ config/config.go
@@ -0,0 +1,33 @@
+package config
+
+import (
+	"fmt"
+
+	"github.com/amarmaulana95/go-echo/pkg"
+	logger "github.com/sirupsen/logrus"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func DatabaseConnect() *gorm.DB {
+	DBURL := fmt.Sprintf("host=%s port=%s user=%s dbname=%s sslmode=disable password=%s", pkg.GodotEnv("DB_HOST"), pkg.GodotEnv("DB_PORT"), pkg.GodotEnv("DB_USER"), pkg.GodotEnv("DB_NAME"), pkg.GodotEnv("DB_PASSWORD"))
+
+	db, err := gorm.Open(postgres.Open(DBURL), &gorm.Config{})
+
+	if err != nil {
+		defer logger.Info("Database connection failed")
+		logger.Fatal(err)
+		return nil
+	}
+
+	// err = db.AutoMigrate(&models.ModelCategory{}, &models.ModelUser{}, &models.ModelSupplier{}, &models.ModelCustomer{})
+	// if err != nil {
+	// 	defer logger.Info("Database connection failed")
+	// 	logger.Fatal(err)
+
+	// 	return nil
+	// }
+
+	return db
+
+}
---
+++ pkg/jwt.go
@@ -0,0 +1,27 @@
+package pkg
+
+import (
+	"strconv"
+	"time"
+
+	"github.com/golang-jwt/jwt"
+)
+
+func Sign(schemajwt *schemas.SchemaJWT) (string, error) {
+
+	expTimeMs, _ := strconv.Atoi("86400000")
+	exp := time.Now().Add(time.Millisecond * time.Duration(expTimeMs)).Unix()
+
+	claims := &schemas.JWtMetaRequest{
+		ID:    schemajwt.ID,
+		Email: schemajwt.Email,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: exp,
+		},
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	jwt, err := token.SignedString([]byte(GodotEnv("JWT_SECRET")))
+
+	return jwt, err
+}
